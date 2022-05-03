a88b6611a4457f374e9088436dcd369d96d3efec
armando bibay jr <apple@apples-MacBook-Pro.local>
2022-05-04 00:21:22 +0800

    crossing out completed tasks

--- src/pages/TodoListPage.js
+++ src/pages/TodoListPage.js
@@ -1,9 +1,21 @@
 import useListOfTodos from "hooks/Implementation/useListOfTodos";
 import React, { ReactNode } from "react";
+import { IUseListOfTodos } from "hooks/Interfaces/IUseListOfTodos";
 
 const TodoListPage = (): ReactNode => {
-  const { data } = useListOfTodos();
-  return <div>{JSON.stringify(data)}</div>;
+  const { data }: IUseListOfTodos = useListOfTodos();
+  return (
+    <div>
+      {data.map(({ id, title, completed }) => (
+        <div
+          style={{ textDecorationLine: completed ? "line-through" : "none" }}
+          key={id}
+        >
+          {title}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default TodoListPage;
